675ad6035c3e3c20691fd1c005b29037041bdb58
João Arthur Fernandes de Macêdo Vieira <20211064010029@ifrn.local>
2024-12-16 17:27:25 -0300

    Projeto Pronto

---
+++ App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        // Criando uma instância de Pessoa para cada estuantes
+        Pessoa pessoa = new Pessoa("Ewerson Ryan", 17);
+
+        // Exibindo as informações dos estudantes
+        pessoa.exibirInformacoes();
+    }
+}
